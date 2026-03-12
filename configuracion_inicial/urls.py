22fe65f08623099ce58204cf90e7ffb7ffde5c8f
Andy <andy@MacBook-Air-de-Andy.local>
2026-03-12 15:13:41 -0600

    Fix: Corrección de rutas de drf-spectacular

--- configuracion_inicial/urls.py
+++ configuracion_inicial/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     # 1. Redirección de Raíz a Swagger
@@ -12,7 +12,7 @@ urlpatterns = [
     
     # 3. Documentación DRF-Spectacular (Swagger)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/', SpectacularRedocView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     
     # 4. Inclusión de las rutas de la App Members
