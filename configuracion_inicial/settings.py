c6d180ae7a477c9d102333dadbabcc61a8e0462b
Andy <andy@MacBook-Air-de-Andy.local>
2026-03-18 12:29:01 -0600

    Token Duration 7 days

--- configuracion_inicial/settings.py
+++ configuracion_inicial/settings.py
@@ -90,7 +90,7 @@ REST_FRAMEWORK = {
 
 # Configuración de JWT
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
